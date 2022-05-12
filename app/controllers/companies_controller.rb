2f4da05b202229bd2ef8a8efb6f1715884c75a99
tomgazzard1 <tom.gazzard@kellogg.northwestern.edu>
2022-05-12 14:42:03 +0000

    Refactored controller for companies and views

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,24 @@
 class CompaniesController < ApplicationController
 
-  # TODO:
-  # def index
-  # end
+  def index
+    @companies = Company.all
+  end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
   end
 
 end
--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,11 @@
 class DiceController < ApplicationController
 
+    def index #This is a special method. It is called an "action" when it is in the controller
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+        @name = params["name"] # Params is pulled from the URL. It is ?name in the URL
+        # render file "views/dice/index.html.erb"
+    end
+
 end
